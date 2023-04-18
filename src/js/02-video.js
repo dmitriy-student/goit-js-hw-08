49baa922ba2d9dab65b514a2be46a4042c88cafb
dmitriy-student <dima.ermol22@gmail.com>
2023-04-19 00:12:20 +0300

    task_2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const player = new Player('vimeo-player');
+const STORE_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(function() {
+    player.getCurrentTime().then(function(seconds) {
+    localStorage.setItem(STORE_KEY, seconds);
+});
+}, 1000));
+
+const savedTime = localStorage.getItem(STORE_KEY);
+if (savedTime) {
+  player.setCurrentTime(savedTime);
+}
