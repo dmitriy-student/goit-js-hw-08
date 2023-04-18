aa39a4d988c0a1cc31a98823bb782967a22815d7
dmitriy-student <dima.ermol22@gmail.com>
2023-04-19 01:10:59 +0300

    task_3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+const emailInput = document.querySelector('[name="email"]');
+const messageInput = document.querySelector('[name="message"]');
+const savedFormState = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+const saveFormState = throttle(() => {
+    const formState = {
+      email: emailInput.value,
+      message: messageInput.value
+    };
+    localStorage.setItem('feedback-form-state', JSON.stringify(formState));
+  }, 500);
+
+formEl.addEventListener('input', saveFormState)
+formEl.addEventListener('submit', onFormSubmit)
+
+if (savedFormState) {
+emailInput.value = savedFormState.email;
+messageInput.value = savedFormState.message;
+}
+
+function onFormSubmit (event) {
+    event.preventDefault();
+
+    localStorage.removeItem('feedback-form-state');
+    emailInput.value = '';
+    messageInput.value = '';
+};
+
+const formState = {
+    email: emailInput.value,
+    message: messageInput.value
+  };
+
+console.log(formState);
